eb9503500e0ed4d5a1913917c3a17fb3efaacc92
LochNeffMonster <atneff@ucsc.edu>
2014-09-14 20:42:53 -0700

    Added a very rough ball throwing prototype to start the project

---
+++ Blue Marble Bocce/Assets/Scripts/BallController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+    private bool buttonHeld = false;
+    private bool throwBall = false;
+    public float forceMinimum = 0.001f;
+    public float forceMaximum = 0.1f;
+    private float forceIncrement = 0.001f;
+    public float throwForce;
+
+	// Use this for initialization
+	void Start () {
+        throwForce = forceMinimum;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space") && !buttonHeld)
+            buttonHeld = true;
+        else if (Input.GetKeyUp("space"))
+        {
+            buttonHeld = false;
+            throwBall = true;
+        }
+
+        if(buttonHeld && (throwForce < forceMaximum)){
+            throwForce += forceIncrement;
+        }
+
+        if(throwBall){
+            rigidbody.AddForce(Vector3.forward * throwForce, ForceMode.Impulse);
+        }
+	}
+}
